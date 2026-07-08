0878708ca31a5e74e7788ea42e68fc7195697d89
Wilko <w.t.wielinga@gmail.com>
2026-07-09 07:20:16 +0800

    Daily Brief - 2026-07-09

--- tools/journal_data.py
+++ tools/journal_data.py
@@ -6,9 +6,9 @@ from _sec_b import AI_HPC
 from _sec_c import CRYPTO_MACRO, MENTAL_HEALTH
 from _sec_d import SPORTS, CONSUMER_TECH
 
-DATE_ISO = "2026-07-08"
-DATE_HUMAN = "Wednesday, July 8, 2026"
-PREV_ISO = "2026-07-07"
+DATE_ISO = "2026-07-09"
+DATE_HUMAN = "Thursday, July 9, 2026"
+PREV_ISO = "2026-07-08"
 MIN_ISO = "2026-03-19"
 
 DATA = {
