0578a617447c41abf19b26ecfd63626bd5a7fd1e
Wilko <w.t.wielinga@gmail.com>
2026-09-05 07:26:29 +0800

    Daily Brief - 2026-09-05

--- tools/journal_data.py
+++ tools/journal_data.py
@@ -3,9 +3,9 @@ import os, json
 _here = os.path.dirname(os.path.abspath(__file__))
 _dd = os.path.join(_here, "data")
 
-DATE_ISO = "2026-09-04"
-DATE_HUMAN = "Friday, September 4, 2026"
-PREV_ISO = "2026-09-03"
+DATE_ISO = "2026-09-05"
+DATE_HUMAN = "Saturday, September 5, 2026"
+PREV_ISO = "2026-09-04"
 MIN_ISO = "2026-03-19"
 
 def _load(name):
